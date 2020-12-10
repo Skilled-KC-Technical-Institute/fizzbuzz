c6fea1af772f425cf5e2314e113269a7a8af94ed
ohampton <44102431+ohampton@users.noreply.github.com>
2020-12-10 15:00:07 -0600

    lineChange

--- main.js
+++ main.js
@@ -4,19 +4,30 @@ for(x=1; x < 101; x++) {
     // divides each number by 3 and 5 and prints Fizzbuzz if its both divisible by 3 and 5
     
     if (x % 3 == 0 && x % 5 == 0) {
-            console.log("FizzBuzz");
+          document.querySelector("#display").innerHTML += "FizzBuzz<br>"; 
+        
+        
+        
+        // console.log("FizzBuzz");
     }
      // divides each number by 3 and prints Fizz if its both divisible by 3 
     else if (x % 3 == 0) {
-        console.log("Fizz");
+        document.querySelector("#display").innerHTML += "Fizz<br>"; 
+        
+        // console.log("Fizz");
     }
      // divides each number by 5 and prints Buzz if its both divisible by 5
     else if (x % 5 == 0) {
-        console.log("Buzz");
+        document.querySelector("#display").innerHTML += "Buzz<br>";
+        
+        // console.log("Buzz");
     }
 
     // Prints the value of x
     else {
-        console.log(x);
+        document.querySelector("#display").innerHTML += x + "<br>" ;
+       
+       
+        // console.log(x);
     }
 }
