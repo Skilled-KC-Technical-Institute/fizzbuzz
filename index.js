6cdee818524a8b17b81fafbfc0fede2073a32328
Carlton-08xx <73480883+Carlton-08xx@users.noreply.github.com>
2020-12-10 02:27:26 -0600

    update fizz

---
+++ index.js
@@ -0,0 +1,13 @@
+let msg = "";
+for (x = 1; x < 101; x++) {
+  if (x % 3 == 0 && x % 5 == 0) msg += "<span class='FizzBuzz'>FizzBuzz</span><br>";
+
+  if (x % 3 == 0) {
+    msg += "<span class='Fizz'>Fizz</span><br>";
+  } else if (x % 5 == 0) {
+    msg += "<span class='Buzz'>Buzz</span><br>";
+  } else {
+    msg += x + "<br>";
+  }
+}
+document.querySelector("#x").innerHTML = msg;
