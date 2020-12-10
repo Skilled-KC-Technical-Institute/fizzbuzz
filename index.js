3e294734ce8362a69a28136bf52c960b9ba1bce7
Carlton-08xx <73480883+Carlton-08xx@users.noreply.github.com>
2020-12-10 02:43:17 -0600

    new branch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let msg = "";
 for (x = 1; x < 101; x++) {
-  if (x % 3 == 0 && x % 5 == 0) msg += "<span class='FizzBuzz'>FizzBuzz</span><br>";
+  if (x % 3 == 0 && x % 5 == 0)
+    msg += "<span class='FizzBuzz'>FizzBuzz</span><br>";
 
   if (x % 3 == 0) {
     msg += "<span class='Fizz'>Fizz</span><br>";
